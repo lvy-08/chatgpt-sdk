3a93808de0d1405038683eb50e2ce73c95c730dc
lvy <1633240719@qq.com>
2024-11-03 21:57:25 +0800

    sdk-gpt4测试

--- src/test/java/cn/bug/chatgpt/test/ApiTest.java
+++ src/test/java/cn/bug/chatgpt/test/ApiTest.java
@@ -1,8 +1,19 @@
 package cn.bug.chatgpt.test;
+
 import cn.bug.chatgpt.common.Constants;
+import cn.bug.chatgpt.domain.billing.BillingUsage;
+import cn.bug.chatgpt.domain.billing.Subscription;
 import cn.bug.chatgpt.domain.chat.ChatCompletionRequest;
 import cn.bug.chatgpt.domain.chat.ChatCompletionResponse;
 import cn.bug.chatgpt.domain.chat.Message;
+import cn.bug.chatgpt.domain.edits.EditRequest;
+import cn.bug.chatgpt.domain.edits.EditResponse;
+import cn.bug.chatgpt.domain.embedd.EmbeddingResponse;
+import cn.bug.chatgpt.domain.files.DeleteFileResponse;
+import cn.bug.chatgpt.domain.files.UploadFileResponse;
+import cn.bug.chatgpt.domain.images.ImageResponse;
+import cn.bug.chatgpt.domain.other.OpenAiResponse;
+import cn.bug.chatgpt.domain.qa.QACompletionRequest;
 import cn.bug.chatgpt.domain.qa.QACompletionResponse;
 import cn.bug.chatgpt.session.Configuration;
 import cn.bug.chatgpt.session.OpenAiSession;
@@ -11,13 +22,24 @@ import cn.bug.chatgpt.session.defaults.DefaultOpenAiSessionFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
 
 /**
+ * @author 小傅哥，微信：fustack
  * @description 单元测试
+ * @github <a href="https://github.com/fuzhengwei">https://github.com/fuzhengwei</a>
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 @Slf4j
 public class ApiTest {
@@ -26,12 +48,12 @@ public class ApiTest {
 
     @Before
     public void test_OpenAiSessionFactory() {
-        // 1. 配置文件【如果你从小傅哥获取key会给你提供apihost，你可以分别替换下】
+        // 1. 配置文件 [联系小傅哥获取key]
+        // 1.1 官网原始 apiHost https://api.openai.com/ - 官网的Key可直接使用
+        // 1.2 三方公司 apiHost https://pro-share-aws-api.zcyai.com/ - 需要找我获得 Key 【支持3.5\4.0流式问答模型调用，有些模型已废弃不对接使用】
         Configuration configuration = new Configuration();
-        configuration.setApiHost("https://pro-share-aws-api.zcyai.com/");;
+        configuration.setApiHost("https://pro-share-aws-api.zcyai.com/");
         configuration.setApiKey("sk-mnpYAt10gxYQctiQF5F7A48f8cB64c6d97F1F1Aa82834fB7");
-        // 可以根据课程首页评论置顶说明获取 apihost、apikey；https://t.zsxq.com/0d3o5FKvc
-        //configuration.setAuthToken("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ4ZmciLCJleHAiOjE2ODMyODE2NzEsImlhdCI6MTY4MzI3ODA3MSwianRpIjoiMWUzZTkwYjYtY2UyNy00NzNlLTk5ZTYtYWQzMWU1MGVkNWE4IiwidXNlcm5hbWUiOiJ4ZmcifQ.YgQRJ2U5-9uydtd6Wbkg2YatsoX-y8mS_OJ3FdNRaX0");
         // 2. 会话工厂
         OpenAiSessionFactory factory = new DefaultOpenAiSessionFactory(configuration);
         // 3. 开启会话
@@ -39,7 +61,75 @@ public class ApiTest {
     }
 
     /**
-     * 简单问答模式，方法废弃推荐使用 test_chat_completions
+     * 【常用对话模式，推荐使用此模型进行测试】
+     * 此对话模型 3.5/4.0 接近于官网体验 & 流式应答
+     */
+    @Test
+    public void test_chat_completions_stream_channel() throws JsonProcessingException, InterruptedException {
+        // 1. 创建参数
+        ChatCompletionRequest chatCompletion = ChatCompletionRequest
+                .builder()
+                .stream(true)
+                .messages(Collections.singletonList(Message.builder().role(Constants.Role.USER).content("1+1").build()))
+                .model(ChatCompletionRequest.Model.GPT_3_5_TURBO.getCode())
+                .maxTokens(1024)
+                .build();
+
+        // 2. 用户配置 【可选参数，支持不同渠道的 apiHost、apiKey】- 方便给每个用户都分配了自己的key，用于售卖场景
+        String apiHost = "https://pro-share-aws-api.zcyai.com/";
+        String apiKey = "sk-mnpYAt10gxYQctiQF5F7A48f8cB64c6d97F1F1Aa82834fB7";
+
+        // 3. 发起请求
+        EventSource eventSource = openAiSession.chatCompletions(apiHost, apiKey, chatCompletion, new EventSourceListener() {
+            @Override
+            public void onEvent(EventSource eventSource, String id, String type, String data) {
+                log.info("测试结果 id:{} type:{} data:{}", id, type, data);
+            }
+
+            @Override
+            public void onFailure(EventSource eventSource, Throwable t, Response response) {
+                log.error("失败 code:{} message:{}", response.code(), response.message());
+            }
+        });
+        // 等待
+        new CountDownLatch(1).await();
+    }
+
+    /**
+     * 【常用对话模式，推荐使用此模型进行测试】
+     * 此对话模型 3.5/4.0 接近于官网体验 & 流式应答
+     */
+    @Test
+    public void test_chat_completions_stream() throws JsonProcessingException, InterruptedException {
+        // 1. 创建参数
+        ChatCompletionRequest chatCompletion = ChatCompletionRequest
+                .builder()
+                .stream(true)
+                .messages(Collections.singletonList(Message.builder().role(Constants.Role.USER).content("1+1").build()))
+                .model(ChatCompletionRequest.Model.GPT_3_5_TURBO.getCode())
+                .maxTokens(1024)
+                .build();
+
+        // 2. 发起请求
+        EventSource eventSource = openAiSession.chatCompletions(chatCompletion, new EventSourceListener() {
+            @Override
+            public void onEvent(EventSource eventSource, String id, String type, String data) {
+                log.info("测试结果 id:{} type:{} data:{}", id, type, data);
+            }
+
+            @Override
+            public void onFailure(EventSource eventSource, Throwable t, Response response) {
+                log.error("失败 code:{} message:{}", response.code(), response.message());
+            }
+        });
+
+        // 等待
+        new CountDownLatch(1).await();
+    }
+
+
+    /**
+     * 简单问答模式
      */
     @Test
     public void test_qa_completions() throws JsonProcessingException {
@@ -48,41 +138,40 @@ public class ApiTest {
     }
 
     /**
+     * 简单问答模式 * 流式应答
+     */
+    @Test
+    public void test_qa_completions_stream() throws JsonProcessingException, InterruptedException {
+        // 1. 创建参数
+        QACompletionRequest request = QACompletionRequest
+                .builder()
+                .prompt("写个java冒泡排序")
+                .stream(true)
+                .build();
+
+        for (int i = 0; i < 1; i++) {
+            // 2. 发起请求
+            EventSource eventSource = openAiSession.completions(request, new EventSourceListener() {
+                @Override
+                public void onEvent(EventSource eventSource, String id, String type, String data) {
+                    log.info("测试结果：{}", data);
+                }
+            });
+        }
+
+        // 等待
+        new CountDownLatch(1).await();
+    }
+
+    /**
      * 此对话模型 3.5 接近于官网体验
-     *
-     * 文档：https://platform.openai.com/docs/guides/text-generation/chat-completions-api
-     * 你可以替换能访问的 apihost【https://api.openai.com】 和 $OPENAI_API_KEY 进行 http 测试
-     * curl https://api.openai.com/v1/chat/completions \
-     *   -H "Content-Type: application/json" \
-     *   -H "Authorization: Bearer $OPENAI_API_KEY" \
-     *   -d '{
-     *     "model": "gpt-3.5-turbo",
-     *     "messages": [
-     *       {
-     *         "role": "system",
-     *         "content": "You are a helpful assistant."
-     *       },
-     *       {
-     *         "role": "user",
-     *         "content": "Who won the world series in 2020?"
-     *       },
-     *       {
-     *         "role": "assistant",
-     *         "content": "The Los Angeles Dodgers won the World Series in 2020."
-     *       },
-     *       {
-     *         "role": "user",
-     *         "content": "Where was it played?"
-     *       }
-     *     ]
-     *   }'
      */
     @Test
     public void test_chat_completions() {
         // 1. 创建参数
         ChatCompletionRequest chatCompletion = ChatCompletionRequest
                 .builder()
-                .messages(Collections.singletonList(Message.builder().role(Constants.Role.USER).content("1+1").build()))
+                .messages(Collections.singletonList(Message.builder().role(Constants.Role.USER).content("写一个java冒泡排序").build()))
                 .model(ChatCompletionRequest.Model.GPT_3_5_TURBO.getCode())
                 .build();
         // 2. 发起请求
@@ -93,5 +182,117 @@ public class ApiTest {
         });
     }
 
-}
+    /**
+     * 上下文对话
+     */
+    @Test
+    public void test_chat_completions_context() {
+        // 1-1. 创建参数
+        ChatCompletionRequest chatCompletion = ChatCompletionRequest
+                .builder()
+                .messages(new ArrayList<>())
+                .model(ChatCompletionRequest.Model.GPT_3_5_TURBO.getCode())
+                .user("testUser01")
+                .build();
+        // 写入请求信息
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content("写一个java冒泡排序").build());
 
+        // 1-2. 发起请求
+        ChatCompletionResponse chatCompletionResponse01 = openAiSession.completions(chatCompletion);
+        log.info("测试结果：{}", chatCompletionResponse01.getChoices());
+
+        // 写入请求信息
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content(chatCompletionResponse01.getChoices().get(0).getMessage().getContent()).build());
+        chatCompletion.getMessages().add(Message.builder().role(Constants.Role.USER).content("换一种写法").build());
+
+        ChatCompletionResponse chatCompletionResponse02 = openAiSession.completions(chatCompletion);
+        log.info("测试结果：{}", chatCompletionResponse02.getChoices());
+    }
+
+    /**
+     * 文本修复
+     */
+    @Test
+    public void test_edit() {
+        // 文本请求
+        EditRequest textRequest = EditRequest.builder()
+                .input("码农会锁")
+                .instruction("帮我修改错字")
+                .model(EditRequest.Model.TEXT_DAVINCI_EDIT_001.getCode()).build();
+        EditResponse textResponse = openAiSession.edit(textRequest);
+        log.info("测试结果：{}", textResponse);
+
+        // 代码请求
+        EditRequest codeRequest = EditRequest.builder()
+                // j <= 10 应该修改为 i <= 10
+                .input("for (int i = 1; j <= 10; i++) {\n" +
+                        "    System.out.println(i);\n" +
+                        "}")
+                .instruction("这段代码执行时报错，请帮我修改").model(EditRequest.Model.CODE_DAVINCI_EDIT_001.getCode()).build();
+        EditResponse codeResponse = openAiSession.edit(codeRequest);
+        log.info("测试结果：{}", codeResponse);
+    }
+
+    /**
+     * 生成图片
+     */
+    @Test
+    public void test_genImages() {
+        // 方式1，简单调用
+        ImageResponse imageResponse01 = openAiSession.genImages("画一个996加班的程序员");
+        log.info("测试结果：{}", imageResponse01);
+
+//        // 方式2，调参调用
+//        ImageResponse imageResponse02 = openAiSession.genImages(ImageRequest.builder()
+//                .prompt("画一个996加班的程序员")
+//                .size(ImageEnum.Size.size_256.getCode())
+//                .responseFormat(ImageEnum.ResponseFormat.B64_JSON.getCode()).build());
+//        log.info("测试结果：{}", imageResponse02);
+    }
+
+    /**
+     * 修改图片，有3个方法，入参不同。
+     */
+    @Test
+    public void test_editImages() throws IOException {
+        ImageResponse imageResponse = openAiSession.editImages(new File("F:\\Web\\Java\\chatgpt\\chatgpt-sdk\\docs\\images\\docs_images_996.png"), "去除图片中的文字");
+        log.info("测试结果：{}", imageResponse);
+    }
+
+    @Test
+    public void test_embeddings() {
+        EmbeddingResponse embeddingResponse = openAiSession.embeddings("哈喽", "嗨", "hi!");
+        log.info("测试结果：{}", embeddingResponse);
+    }
+
+    @Test
+    public void test_files() {
+        OpenAiResponse<File> openAiResponse = openAiSession.files();
+        log.info("测试结果：{}", openAiResponse);
+    }
+
+    @Test
+    public void test_uploadFile() {
+        UploadFileResponse uploadFileResponse = openAiSession.uploadFile(new File("/Users/fuzhengwei/1024/KnowledgePlanet/chatgpt/chatgpt-sdk-java/docs/files/introduce.md"));
+        log.info("测试结果：{}", uploadFileResponse);
+    }
+
+    @Test
+    public void test_deleteFile() {
+        DeleteFileResponse deleteFileResponse = openAiSession.deleteFile("file id 上传后才能获得");
+        log.info("测试结果：{}", deleteFileResponse);
+    }
+
+    @Test
+    public void test_subscription() {
+        Subscription subscription = openAiSession.subscription();
+        log.info("测试结果：{}", subscription);
+    }
+
+    @Test
+    public void test_billingUsage() {
+        BillingUsage billingUsage = openAiSession.billingUsage(LocalDate.of(2023, 3, 20), LocalDate.now());
+        log.info("测试结果：{}", billingUsage.getTotalUsage());
+    }
+
+}
